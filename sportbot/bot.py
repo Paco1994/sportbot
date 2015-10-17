9f525f1bf7d63a31201740a2d410c4832ed8c581
i32roraj <i32roraj@uco.es>
2015-10-17 13:00:15 +0200

    Añadido el comando start.

--- sportbot/bot.py
+++ sportbot/bot.py
@@ -14,8 +14,8 @@ TOKEN = '163718613:AAHUpvFTGS2xkmHQsIDKzWe4uEnlLsBQFrQ' # Nuestro tokken del bot
 bot = telebot.TeleBot(TOKEN) # Creamos el objeto de nuestro bot.
 local = True
 gif = "https://lachatupdate.files.wordpress.com/2015/08/547b7a894bcc7.gif"
-usuarios = [line.rstrip('\n') for line in open('usuarios.txt')] #cargamos la lista de usuarios
-administrador = 14550586
+usuarios = [line.rstrip('\n') for line in open('sources/usuarios.txt')] #cargamos la lista de usuarios
+administrador = '43748337'
 
 def listener(messages):
     for m in messages:
@@ -38,6 +38,24 @@ def listener(messages):
 
 bot.set_update_listener(listener) # Así, le decimos al bot que utilice como función escuchadora nuestra función 'listener' declarada arriba.
 
-
+@bot.message_handler(commands=['start'])
+def command_start(m):
+    cid = m.chat.id
+    if str(cid) in usuarios:
+        bot.send_message( cid, "Ya estás registrado como usuario del ASL Sports Bot." )
+    else: # Con esta sentencia, hacemos que solo se ejecute lo de abajo cuando un usuario hace uso del bot por primera vez.
+        usuarios.append(str(cid)) # En caso de no estar en la lista de usuarios, lo añadimos.
+        f = open( 'sources/usuarios.txt', 'a') # Y lo insertamos en el fichero 'usuarios.txt'
+        f.write( str(cid) + "\n")
+        f.close()
+        bot.send_message( administrador, "Se ha registrado el ID " + str(cid))
+        #USERFILE = "users/" + str(cid) + ".txt"
+        #f = open( USERFILE, 'w')
+        #f.close()
+        if cid < 0:
+            bot.send_message( cid, "Bienvenidos. Este grupo se ha registrado en ASL Sports Bot.")
+        else:
+            bot.send_message( cid, "Bienvenido. Te has registrado en ASL Sports Bot. Elige tu equipo favorito.")
+			#ELEGIR EQUIPO.
 
 bot.polling(none_stop=True) # Con esto, le decimos al bot que siga funcionando incluso si encuentra algún fallo.
